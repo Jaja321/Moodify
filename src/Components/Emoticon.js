9499d6bddb85dae2505e57da48864fbbb2bb97d4
Jaja321 <benjajamin@gmail.com>
2020-05-09 20:26:26 +0300

    refactor Emoticon

--- src/Components/Emoticon.js
+++ src/Components/Emoticon.js
@@ -1,37 +1,35 @@
 import React from "react";
 
-export default ({ valence, size }) => {
+const valenceToType = {
+  0.2: "sentiment_very_dissatisfied",
+  0.4: "sentiment_dissatisfied",
+  0.6: "sentiment_satisfied",
+  0.8: ["sentiment_satisfied", true],
+  1: "sentiment_very_satisfied",
+};
+
+const getType = (valence) => {
+  const thresholds = Object.keys(valenceToType).sort();
+  for (let i = 0; i < thresholds.length; i++) {
+    if (valence < thresholds[i]) {
+      const type = valenceToType[thresholds[i]];
+      return Array.isArray(type) ? type : [type, false];
+    }
+  }
+};
+
+const Icon = ({ size, type, outlined }) => {
   const style = {
     fontSize: size + "px",
   };
-  if (valence < 0.2)
-    return (
-      <i className="material-icons" style={style}>
-        sentiment_very_dissatisfied
-      </i>
-    );
-  else if (valence < 0.4)
-    return (
-      <i className="material-icons" style={style}>
-        sentiment_dissatisfied
-      </i>
-    );
-  else if (valence < 0.6)
-    return (
-      <i className="material-icons" style={style}>
-        sentiment_satisfied
-      </i>
-    );
-  else if (valence < 0.8)
-    return (
-      <i className="material-icons-outlined" style={style}>
-        sentiment_satisfied
-      </i>
-    );
-  else
-    return (
-      <i className="material-icons" style={style}>
-        sentiment_very_satisfied
-      </i>
-    );
+  return (
+    <i className={outlined ? "material-icons-outlined" : "material-icons"} style={style}>
+      {type}
+    </i>
+  );
+};
+
+export default ({ valence, size }) => {
+  const [type, outline] = getType(valence);
+  return <Icon type={type} size={size} outline={outline} />;
 };
