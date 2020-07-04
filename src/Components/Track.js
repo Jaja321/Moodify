3411563202dfdc3a0b78745f0d06bf77e10570eb
Jaja321 <benjajamin@gmail.com>
2020-07-04 20:09:38 +0300

    show album image

--- src/Components/Track.js
+++ src/Components/Track.js
@@ -1,33 +1,55 @@
-import React, { useState } from "react";
-import Emoticon from "./Emoticon";
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  max-width: 250px;
+  display: flex;
+  height: 80px;
+  align-items: center;
+`;
+
+const TrackDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-left: 8px;
+`;
+
+const Thumbnail = styled.img`
+  height: 45px;
+  width: 45px;
+  border-radius: 5px;
+`;
+
+const ArtistLabel = styled.div`
+  font-size: 14px;
+`;
+
+const TrackNameLabel = styled.div`
+  font-size: 20px;
+  font-weight: bold;
+`;
 
 const formatTrackName = (trackName) => {
   const maxLength = 35;
   if (trackName.length > maxLength) {
-    return trackName.substring(0, maxLength - 3) + "...";
+    return trackName.substring(0, maxLength - 3) + '...';
   } else {
     return trackName;
   }
 };
 
 export default ({ idx, trackData }) => {
-  const [hover, setHover] = useState(false);
+  //const [hover, setHover] = useState(false);
+  console.log(trackData);
 
   return (
-    <div
-      className="track"
-      onMouseEnter={() => setHover(true)}
-      onMouseLeave={() => setHover(false)}
-    >
-      {hover ? (
-        <Emoticon valence={trackData.valence} size={50} />
-      ) : (
-        <div className="trackIndex">{idx}</div>
-      )}
-      <div className="trackDetails">
-        <div className="artist">{trackData.artists[0].name}</div>
-        <div className="trackName">{formatTrackName(trackData.name)}</div>
-      </div>
-    </div>
+    <Wrapper>
+      <Thumbnail src={trackData.album.images[2].url} />
+      <TrackDetails>
+        <ArtistLabel>{trackData.artists[0].name}</ArtistLabel>
+        <TrackNameLabel>{formatTrackName(trackData.name)}</TrackNameLabel>
+      </TrackDetails>
+    </Wrapper>
   );
 };
