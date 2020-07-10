1044b06763b608c69da09096ec95497faecba671
Jaja321 <benjajamin@gmail.com>
2020-07-10 17:49:46 +0300

    remove ResultsScreen

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Home from "./Home";
-import ResultsScreen from "./ResultsScreen";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import SelectorScreen from "./SelectorScreen";
 
