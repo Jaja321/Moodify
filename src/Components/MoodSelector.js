3596a1c57050207fbeb0a5e731544a0138c2193c
Jaja321 <benjajamin@gmail.com>
2020-07-11 15:19:43 +0300

    make selector responsive

--- src/Components/MoodSelector.js
+++ src/Components/MoodSelector.js
@@ -1,16 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import { useWindowWidth } from '@react-hook/window-size';
 import { useDispatch } from 'react-redux';
 import { setAudioProperties } from '../reducer';
 import { getRecommendations } from '../actions';
 import Emoticon from './Emoticon';
 
-const SIZE = 300;
-const CIRCLE_SIZE = 40;
-
 const Square = styled.div`
-  height: ${SIZE}px;
-  width: ${SIZE}px;
+  height: ${({ size }) => size}px;
+  width: ${({ size }) => size}px;
   border: 1px solid white;
   border-radius: 20px;
   margin-top: 2rem;
@@ -18,89 +16,104 @@ const Square = styled.div`
   margin-bottom: 36px;
 `;
 
-const xLabel = styled.div`
-  width: 100%;
-  text-align: center;
+const Label = styled.div`
+  font-size: ${({ fontSize }) => fontSize}px;
 `;
 
-const yLabel = styled.div`
+const xLabel = styled(Label)`
+  width: 100%;
+  text-align: center;
+  user-select: none;
+`;
+
+const yLabel = styled(Label)`
   position: absolute;
-  top: ${SIZE / 2 - 8}px;
+  top: ${({ squareSize }) => squareSize / 2 - 8}px;
+  user-select: none;
 `;
 
 const TopLabel = styled(xLabel)`
   position: relative;
-  top: -1.5rem;
+  top: ${({ fontSize }) => -1.4 * fontSize}px;
 `;
 
 const BottomLabel = styled(xLabel)`
   position: absolute;
-  top: ${SIZE + 5}px;
+  top: ${({ squareSize }) => squareSize + 5}px;
 `;
 
 const LeftLabel = styled(yLabel)`
-  left: -35px;
+  left: ${({ fontSize }) => -2.1 * fontSize}px;
 `;
 
 const RightLabel = styled(yLabel)`
-  left: ${SIZE + 5}px;
+  left: ${({ squareSize }) => squareSize + 5}px;
 `;
 
 const EmoticonWrapper = styled.div`
   position: absolute;
-  ${({ x, y }) => `
-    top: ${y - CIRCLE_SIZE / 2}px;
-    left: ${x - CIRCLE_SIZE / 2}px;
+  ${({ x, y, squareSize, circleSize }) => `
+    top: ${y * squareSize - circleSize / 2}px;
+    left: ${x * squareSize - circleSize / 2}px;
   `}
   cursor: pointer;
+  touch-action: none;
 `;
 
-const normalizeCoord = (coord) => Math.min(Math.max(0 + CIRCLE_SIZE / 2, coord), SIZE - CIRCLE_SIZE / 2);
-
-const coordToValue = (value) => value / SIZE;
+const normalizeCoord = (coord, squareSize, circleSize) =>
+  Math.min(Math.max(0 + circleSize / 2, coord), squareSize - circleSize / 2) / squareSize;
 
 export default () => {
   const dispatch = useDispatch();
   const [isPointerDown, setPointerDown] = useState(false);
-  const [{ x, y }, setCoords] = useState({ x: SIZE / 2, y: SIZE / 2 });
+  const [{ x, y }, setCoords] = useState({ x: 0.5, y: 0.5 });
+  const windowWidth = useWindowWidth();
+  const squareSize = Math.min(300, 0.7 * windowWidth);
+  const circleSize = squareSize / 7.5;
 
   useEffect(() => {
     const mouseUpHandler = () => {
       if (isPointerDown) {
         setPointerDown(false);
         const audioProperties = {
-          valence: coordToValue(x),
-          energy: 1 - coordToValue(y),
+          valence: x,
+          energy: 1 - y,
         };
         dispatch(setAudioProperties(audioProperties));
         dispatch(getRecommendations());
       }
     };
-    document.addEventListener('mouseup', mouseUpHandler);
-    return () => document.removeEventListener('mouseup', mouseUpHandler);
+    document.addEventListener('pointerup', mouseUpHandler);
+    return () => document.removeEventListener('pointerup', mouseUpHandler);
   }, [isPointerDown, x, y, dispatch]);
+
+  const labelProps = {
+    squareSize,
+    fontSize: squareSize / 18.5,
+  };
 
   return (
     <Square
-      onMouseDown={(e) => {
+      onPointerDown={(e) => {
         e.preventDefault();
         setPointerDown(true);
       }}
-      onMouseMove={(e) => {
+      onPointerMove={(e) => {
         if (isPointerDown) {
           const rect = e.currentTarget.getBoundingClientRect();
-          const x = normalizeCoord(e.clientX - rect.left);
-          const y = normalizeCoord(e.clientY - rect.top);
+          const x = normalizeCoord(e.clientX - rect.left, squareSize, circleSize);
+          const y = normalizeCoord(e.clientY - rect.top, squareSize, circleSize);
           setCoords({ x, y });
         }
       }}
+      size={squareSize}
     >
-      <TopLabel>Energetic</TopLabel>
-      <BottomLabel>Calm</BottomLabel>
-      <LeftLabel>Sad</LeftLabel>
-      <RightLabel>Happy</RightLabel>
-      <EmoticonWrapper x={x} y={y} pointerDown={isPointerDown}>
-        <Emoticon size={CIRCLE_SIZE} valence={x / SIZE} />
+      <TopLabel {...labelProps}>Energetic</TopLabel>
+      <BottomLabel {...labelProps}>Calm</BottomLabel>
+      <LeftLabel {...labelProps}>Sad</LeftLabel>
+      <RightLabel {...labelProps}>Happy</RightLabel>
+      <EmoticonWrapper x={x} y={y} pointerDown={isPointerDown} squareSize={squareSize} circleSize={circleSize}>
+        <Emoticon size={circleSize} valence={x} />
       </EmoticonWrapper>
     </Square>
   );
