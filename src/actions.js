104441a35e6cf51128a7b6b92237fcf41ba76eba
Jaja321 <benjajamin@gmail.com>
2020-07-22 22:07:40 +0300

    remove unused code

--- src/actions.js
+++ src/actions.js
@@ -1,9 +1,6 @@
 import { setTracks, setLoading } from './reducer';
 
-const audioFeaturesUri = 'https://api.spotify.com/v1/audio-features';
-const topTracksUri = 'https://api.spotify.com/v1/me/top/tracks?limit=50';
 const baseRecommendationsUri = 'https://api.spotify.com/v1/recommendations';
-const timeRanges = ['short_term', 'medium_term', 'long_term'];
 
 const getHeaders = (accessToken) => ({ Authorization: 'Bearer ' + accessToken });
 
@@ -14,49 +11,12 @@ const getRecommendationsUri = (audioProperties, selectedGenres) => {
   const minEnergy = Math.max(0, energy - 0.1);
   const maxEnergy = Math.min(1, energy + 0.1);
   const minValence = Math.max(0, valence - 0.1);
-  const maxValence = Math.min(1, valence + 0.1);
+  const maxValence = Math.min(1, valence + 0.1);  
   const genreList = selectedGenres.map(genre => genre.toLowerCase()).join(',');
   return `${baseRecommendationsUri}?seed_genres=${genreList}&min_energy=${minEnergy}&max_energy=${maxEnergy}&min_valence=${minValence}&max_valence=${maxValence}`;
 };
 
-const getAudioFeatures = async (ids, headers) => {
-  return (
-    await fetch(`${audioFeaturesUri}?ids=${getTracksIdList(ids)}`, { headers }).then((response) => response.json())
-  ).audio_features;
-};
-
-const getTracksIdList = (tracks) => tracks.map((track) => track.id).join(',');
-
-export const getTracks = () => async (dispatch, getState) => {
-  const { accessToken } = getState();
-  const headers = getHeaders(accessToken);
-  const topTracksSet = {};
-  (
-    await Promise.all(
-      timeRanges.map((timeRange) =>
-        fetch(`${topTracksUri}&time_range=${timeRange}`, { headers }).then((response) => response.json())
-      )
-    )
-  ).forEach((tracks) => {
-    tracks.items.forEach((track) => {
-      topTracksSet[track.id] = track;
-    });
-  });
-  const topTracks = Object.values(topTracksSet);
-  const firstPart = await getAudioFeatures(topTracks.slice(0, 99), headers);
-  const secondPart = await getAudioFeatures(topTracks.slice(99, 150), headers);
-  const audioFeatures = [...firstPart, ...secondPart];
-  audioFeatures.forEach((track, i) => {
-    topTracks[i].valence = track.valence;
-    topTracks[i].energy = track.energy;
-  });
-  dispatch(setTracks(topTracks));
-  dispatch(setLoading(false));
-};
-
 export const getRecommendations = () => async (dispatch, getState) => {
-
-  //const accessToken = qs.parse(location.hash, { ignoreQueryPrefix: true }).access_token;
   const { accessToken, selectedGenres, audioProperties } = getState();
   if(selectedGenres.length === 0) {
     return null;
@@ -69,10 +29,13 @@ export const getRecommendations = () => async (dispatch, getState) => {
     window.location.href = '/';
     return;
   }
-  console.log(
-    'recommendations',
-    res.tracks.map((track) => track.name + ' - ' + track.artists[0].name)
-  );
-  dispatch(setTracks(res.tracks)); //TODO: map the tracks to only include relevant data.
+  const tracks = res.tracks.map(track => ({
+    name: track.name,
+    artist: track.artists[0].name,
+    id: track.id,
+    imageUrl: track.album.images[2].url,
+    uri: track.uri
+  }));
+  dispatch(setTracks(tracks));
   dispatch(setLoading(false));
 };
